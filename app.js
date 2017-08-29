e29516bdaf0f40840775a06c1f148cc5d8fcd750
Michael Montero <mmontero@agilisa.com>
2017-08-29 14:33:12 -0400

    Add actions buttons

--- app.js
+++ app.js
@@ -39,7 +39,7 @@
         showVehicles();
     }
 
-
+    //Fills the table that contains the information dynamically
     function showVehicles(){
         var dataLocation = document.getElementById("tBody");
         dataLocation.innerHTML = "";
@@ -53,10 +53,37 @@
                 td.innerHTML = property;
                 tr.appendChild(td)
             }
+            tdAction = dce("td");
+
+            delButton = createButton("Delete", "btn btn-danger", "deleteVicle("+x+")"); //Create delete button
+            editButton = createButton("Edit", "btn btn-warning","editVicle("+x+")"); //Create edit button
+
+            tdAction.appendChild(delButton);
+            tdAction.appendChild(editButton);
+
+            tr.appendChild(tdAction)
             dataLocation.appendChild(tr)
         }
     }
 
+    //Return a new button
+    function createButton(text, className, functionName){
+        var btn = dce("button");
+        btn.type = "button";
+        btn.setAttribute("class", className);
+        btn.innerHTML = text;
+        btn.setAttribute("onClick", functionName)
+        return btn;
+    }
+
     function dce(e){
         return document.createElement(e);
+    }
+
+    function deleteVicle(index){
+
+    }
+
+    function editVicle(index){
+        
     }
