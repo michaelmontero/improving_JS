77292f246bc8b1806e6aaf36686bd5503719dd1b
Michael Montero <mmontero@agilisa.com>
2017-08-29 13:55:03 -0400

    Save the vehicle object into localStorage

---
+++ app.js
@@ -0,0 +1,29 @@
+    
+    var data = {
+        vehicle : [],
+        brand : [],
+        model : []
+    };
+    var objVehicle = null;
+
+    //this function insert the vehicle object into the data.vehicle array
+    function saveVehiche(){
+        var carId = $("#carId").val();
+        var brand = $("#brand").val();
+        var model = $("#model").val();
+        var transmission = $("#transmission").val();
+        var commentary = $("#commentary").val();
+
+
+        //Create a vehicle obj
+        objVehicle = {"carId" : carId,"brand" : brand, "model" : model,"transmission" : transmission,"commentary" : commentary};
+        data.vehicle.push(objVehicle);
+
+        //insert the method in the local storage
+        saveAll();
+    }
+
+    //Insert the data object in localStorage
+    function saveAll(){
+        localStorage.setItem("data", JSON.stringify(data));
+    }
